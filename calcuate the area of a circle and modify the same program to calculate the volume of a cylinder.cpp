c7dd31e80c6bebf8bdba8f06709b0264699f86b9
lakshyaberia1 <lakshyaberia1@gmail.com>
2024-11-23 23:38:07 -0800

    Add files via upload

---
+++ calcuate the area of a circle and modify the same program to calculate the volume of a cylinder.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+	 float a,h,r;
+	printf("enter radius: ");
+	scanf("%f",&r);
+		printf("enter lenght: ");
+			scanf("%f",&h);
+	//printf("area of circle is %d",3.14*r*r);
+	a=r*r*h*3.14;
+		printf("area of cylender is %f",a);
+	return 0;
+	
+	
+	
+}
