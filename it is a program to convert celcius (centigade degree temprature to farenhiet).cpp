c25b002fe9f705fccbb8bb68af7f4032d0430761
lakshyaberia1 <lakshyaberia1@gmail.com>
2024-11-23 23:43:02 -0800

    Add files via upload

---
+++ it is a program to convert celcius (centigade degree temprature to farenhiet).cpp
@@ -0,0 +1,10 @@
+// it is a program to convert celcius (centigade degree temprature to farenhiet)
+#include <stdio.h>
+
+int main(){
+	int c;
+	printf("enter temprature in celsius: ");
+		scanf("%d",&c);
+		printf("%d c in fehrenheit is %f: \n",c,(9.0/5.0*c)+32);
+
+}
